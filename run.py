fe7588fe7de8af166ffd1d70344ddaedd879bbee
GcWan <GcWan@users.noreply.github.com>
2020-06-10 14:20:21 -0700

    Update

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+import search
+
 
 # export FLASK_APP
 app = Flask(__name__)
@@ -9,15 +11,15 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
+    params = body.split()
 
     # Start our TwiML response
     resp = MessagingResponse()
 
     # Add a message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
+    if params[0].lower() == 'google':
+        result = search.googleSearch(body[7:])
+        resp.message("Here is Google's top result:\n"+result)
     else:
         resp.message("The Robots are coming! Head for the hills!")
 
--- search.py
+++ search.py
@@ -1,15 +1,6 @@
-from bs4 import BeautifulSoup
+from googlesearch import search
 
 
-def googleSearch(terms):
-    url = "https://www.google.ca/search?q={}".format("%20".join(terms))
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, features="html.parser")
-    result = soup.find('div', class_='Z0LcW XcVN5d')
-    return(result.text)
-
-query = "When was Obama Born?"
-response = requests.get("https://www.google.ca/search?q="+"%20".join(query.split()))
-soup = BeautifulSoup(response.text, features="html.parser")
-result = soup.find('div', class_='BNeawe iBp4i AP7Wnd')
-print('testing 1 2 3')
+def googleSearch(query):
+    for r in search(query, tld='ca', lang='en', num=1, start=0, stop=1, pause=2.0):
+        return r
